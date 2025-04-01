3f5d86beefaf45ed083dff369abbcf44fc5fe89e
Christopher S. Vesterby <vesterby@hotmail.dk>
2025-04-01 13:56:32 +0200

    Er i gang med at forsøge at anvende den nødvendige produktformular.

--- frederik/product.js
+++ frederik/product.js
@@ -1,5 +1,5 @@
 const products = [
-    { id: 1, name: "Hyggetæppe", description: "Fucking hyggeligt bro og broinde!", image:"1-5e5a8068.jpg", price: 199, featured: true },
+    { id: 1, name: "Hyggetæppe", description: "Fucking hyggeligt bro og broinde!", image: "1-5e5a8068.jpg", price: 199, featured: true },
     { id: 2, name: "Cardigan i kameluld", description: "En lækker Cardigan", image: "1.jpg", price: 299.99, featured: true },
     { id: 3, name: "Festkjole", description: "Perfekt til at danse i", image: "2.jpg", price: 299.99, featured: true },
     { id: 4, name: "Konfirmationskjole", description: "Skal du konfirmeres? Så er det der kjolen du skal have!", image: "3.jpg", price: 299.99, featured: true },
@@ -8,6 +8,8 @@ const products = [
     { id: 7, name: "Heavy Duty T-shirt", description: "Til knap så kolde dage", image: "6.jpg", price: 299.99, featured: true },
     { id: 8, name: "Slim fit Jeans", description: "Til knap så varme dage", image: "7.jpg", price: 299.99, featured: true }
 ];
+
+// Anvend produkt formular fra Niels's eksempel på Teams. Den skal bruges til funktionaliteten til min kurv.
 
 const cart = [];
 
